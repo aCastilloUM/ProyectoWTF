51ae73d8f151b1f76aa040f5c64252c9c9546b17
aCastilloUM <a.castillo@correo.um.edu.uy>
2024-11-17 22:28:11 -0300

    Arrelo SeatsController

--- src/main/java/edu/uy/um/wtf/controllers/entitiesControllers/seatsController.java
+++ src/main/java/edu/uy/um/wtf/controllers/entitiesControllers/seatsController.java
@@ -1,19 +1,41 @@
 package edu.uy.um.wtf.controllers.entitiesControllers;
 
+import edu.uy.um.wtf.entities.Seats;
+import edu.uy.um.wtf.services.SeatsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/seats")
 public class SeatsController {
 
-    @GetMapping("/showSeats")
-    public String mostrarAsientos(Model model) {
-        boolean[][] asientos = new boolean[15][10];  // Todos los asientos disponibles
-        model.addAttribute("asientos", asientos);
-        return "seats";
+    @Autowired
+    private SeatsService seatsService;
 
+    @GetMapping("/showSeats/{filmShowId}")
+    public String showSeats(@PathVariable Long filmShowId, Model model) {
+        List<Seats> asientos = seatsService.getSeatsByFilmShowId(filmShowId);
+        List<List<Seats>> filas = new ArrayList<>();
+        for (int i = 0; i < asientos.size(); i += 10) {
+            filas.add(asientos.subList(i, Math.min(i + 10, asientos.size())));
+        }
+
+        model.addAttribute("asientos", filas);
+        model.addAttribute("filmShowId", filmShowId);
+        return "seats";
+    }
+
+    @PostMapping("/confirmReservation")
+    public String confirmReservation(Long filmShowId, List<Long> selectedSeats) {
+        seatsService.reserveSeats(filmShowId, selectedSeats);
+        return "redirect:/seats/showSeats/" + filmShowId;
     }
 }
